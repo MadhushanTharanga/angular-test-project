0b26c5c90ca17a50e2cca9b99643b49fb6868079
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-05 22:37:15 +0530

    updated!!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {ServiceComponent} from "./components/service/service.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'**',component:NotFoundPageComponent},
+];
