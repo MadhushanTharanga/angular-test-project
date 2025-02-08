1e0227b0daa6db37512e7f9d152d21d0768cf11c
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-08 18:24:34 +0530

    updated!!

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,18 +3,29 @@ import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {debounceTime, Observable} from "rxjs";
 import {response} from "express";
 import {log} from "util";
+import {CurrencyPipe, DatePipe, JsonPipe, SlicePipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UpperCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    SlicePipe,
+    JsonPipe
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements   OnInit{
+  text = 'Hi suduuuu';
+  date=new Date();
+  salary=1234.99;
+  name = 'kamal chandana';
+  user={name:'kamal',age:24}
   searchText = new FormControl();
   ngOnInit(): void {
     // this.searchText.valueChanges.subscribe((value)=>{
