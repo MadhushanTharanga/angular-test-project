165ca54ab902c003b30bd5b8b61ec9a2aef6e910
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-08 18:43:25 +0530

    updated!!

---
+++ src/app/module/order/components/order-default/order-default.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-default',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-default.component.html',
+  styleUrl: './order-default.component.scss'
+})
+export class OrderDefaultComponent {
+
+}
