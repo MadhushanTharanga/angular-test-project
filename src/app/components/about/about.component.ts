8d7b728ad7f4fd20eb09ff24faf3339caf7e2765
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-08 10:27:25 +0530

    updated!!

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Observable} from "rxjs";
+import {response} from "express";
+import {log} from "util";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-  text='';
+export class AboutComponent implements   OnInit{
+  searchText = new FormControl();
+  ngOnInit(): void {
+    // this.searchText.valueChanges.subscribe((value)=>{
+    //   console.log(`will make the request [${value}`)
+    // })
+
+    this.searchText.valueChanges.pipe(
+      
+    )
+  }
+
 }
